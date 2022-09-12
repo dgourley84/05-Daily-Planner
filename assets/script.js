3b3245fe93dbcc89557e33220797f7149274f7a9
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-09-12 19:40:48 +1000

    Error in commit didng branch before starting

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+function createTimeBlock(){
+// <div class="row align-items-center time-row">
+//  <div class="col-2 d-flex justify-content-center align-items-center time-col-time">9am</div>
+//  <div class="col-8 time-col-textarea">
+//      <textarea name="" id="" cols="60" rows="2"></textarea>
+//  </div>
+//  <div class="col-2 time-col-button">                   
+//     <button type="button" class="btn btn-primary">Save</button>
+//  </div>
+// </div>
+}
