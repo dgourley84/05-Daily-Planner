d9d23bd47aa1ca56908711d15ddd199657ef4d4c
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-09-13 16:40:52 +1000

    Timeblock creation in HTML usin JS.

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,32 @@
 
 
 
-function createTimeBlock(time){
-// <div class="row align-items-center time-row">
-//  <div class="col-2 d-flex justify-content-center align-items-center time-col-time">9am</div>
-//  <div class="col-8 time-col-textarea">
-//      <textarea name="" id="" cols="60" rows="2"></textarea>
-//  </div>
-//  <div class="col-2 time-col-button">                   
-//     <button type="button" class="btn btn-primary">Save</button>
-//  </div>
-// </div>
-}
+function createTimeBlock(time, content = ""){
 
+//create time row in the timeblock
+const timeRow = $("<div class='row align-items-center time-row'>");
+//create the time in the timeblock using 24hour time
+const colTime = $("<div class='col-2 d-flex justify-content-center align-items-center time-col-time'>").text(time + ":00");
+//create text area input values in time block
+const colTextArea = $("<div class='col-8 time-col-textarea'>");
+const textArea = $("<textarea cols='60' rows='2'>").val(content);
+colTextArea.append(textArea);
+//create save button in timeblock
+const colButton = $("<div class ='col-2 time-col-button'>");
+const button = $("<button type='button' class='btn btn-primary'>").text("Save");
+colButton.append(button);
+//create the timeblock line.
+return timeRow.append(colTime, colTextArea, colButton);
+
+}
+//create the time function to be prestented in the jumbotron at top of page
 $(function(){
     const currentTime = $("#current-time");
     const container = $(".container");
     
     function startTimer(){      
         setInterval(function(){
-            currentTime.text(moment().format("YYYY-MM-DD HH:mm:ss"))
+            currentTime.text(moment().format("YYYY-MM-DD HH:mm:ss"));
         },1000);
     }
     //when user opens this application
@@ -30,14 +37,14 @@ $(function(){
     //user should be presented with 7am to 7pm time blocks
     //these blocks should be color coded -past present future
     //all previous notes
-
-    for (let index = 12; index < 18; index++){
+    //iterate the time blocks for 12 hours from 8am until 10pm in 24 hour time
+    for (let index = 8; index < 21; index++){
 
     const previousNotes = "";
     
     const timeBlock = createTimeBlock(index,previousNotes);
 
-    container.appent(timeBlock);
+    container.append(timeBlock);
 
 
     }
